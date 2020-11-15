4535c4f7ebf4d9864702cb8a0775a957ca28777a
yangd <yangd@2345.com>
2020-11-15 10:51:06 +0800

    first commit

---
+++ 05_array/Array.java
@@ -0,0 +1,11 @@
+package beauty_of_algorithm.05_array;
+
+public class Array {
+    public static void main(String[] args) {
+        print("Hello World!");
+    }
+
+    public static void print(String s) {
+        System.out.println(s);
+    }
+}
